942e0467d6c86efcda1cb57ca9e1d6a90c3adde2
Jin <it.minhhung@gmail.com>
2019-05-02 05:42:31 +0700

    Set timeout for a long running process

---
+++ TaskTimeoutForLongRunningProcess/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://exprotocol.com/2017/02/15/set-timeout-for-a-long-running-process-using-c/
+/// </summary>
+namespace TaskTimeoutForLongRunningProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var activity = new Activity();
+            activity.Setup().Wait();
+
+            Console.ReadLine();
+        }
+
+        public class Activity
+        {
+            private CancellationTokenSource tokenSource;
+
+            public async Task Setup()
+            {
+                TimeSpan timeoutValue = new TimeSpan(0, 0, 2);
+
+                tokenSource = new CancellationTokenSource();
+
+                var task = Task.Run(() => Run(), tokenSource.Token);
+
+                //Check the task is delaying
+                if (await Task.WhenAny(task, Task.Delay(timeoutValue)) == task)
+                {
+                    // task completed within the timeout
+                    Console.WriteLine("Task Completed Successfully");
+                }
+                else
+                {
+                    // timeout => cancel the task
+                    tokenSource.Cancel();
+
+                    Console.WriteLine("Time Out. Aborting Task");
+
+                    task.GetAwaiter().GetResult(); //Waiting for the task to throw OperationCanceledException
+                }
+            }
+
+            /// <summary>
+            /// Long running process
+            /// </summary>
+            public void Run()
+            {
+                int action = 1;
+
+                try
+                {
+                    while (true)
+                    {
+                        if (tokenSource.Token.IsCancellationRequested)
+                            tokenSource.Token.ThrowIfCancellationRequested();  //Stop if the cancellation requested
+
+                        Console.WriteLine("Running action " + action++);
+
+                        Thread.Sleep(200);
+                    }
+
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Task Aborted");
+                }
+            }
+        }
+    }
+}
