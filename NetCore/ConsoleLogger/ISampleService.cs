d42f785b502fe575b6abf1c3a5ceca78b9839536
Jin <it.minhhung@gmail.com>
2020-08-13 19:31:48 +0700

    .net core console app with logger using serilog + DI

---
+++ NetCore/ConsoleLogger/ISampleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleLogger
+{
+    public interface ISampleService
+    {
+        void DoSomething();
+    }
+}
---
+++ NetCore/ConsoleLogger/SampleService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleLogger
+{
+    public class SampleService : ISampleService
+    {
+        private readonly ILogger<SampleService> _log;
+        private readonly IConfiguration _config;
+
+        public SampleService(ILogger<SampleService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void DoSomething()
+        {
+            var countdownStartValue = _config.GetValue<int>("CountdownStartValue");
+            var countdownTick = _config.GetValue<int>("CountdownTick");
+
+            for (int i = countdownStartValue; i >= 0; i -= countdownTick)
+            {
+                if (i % 10 == 0)
+                {
+                    _log.LogError($"Current value {i.ToString().PadLeft(countdownStartValue.ToString().Length, ' ')}");
+                }
+                else
+                {
+                    _log.LogWarning($"Current value {i.ToString().PadLeft(countdownStartValue.ToString().Length, ' ')}");
+                }
+            }
+        }
+    }
+}
